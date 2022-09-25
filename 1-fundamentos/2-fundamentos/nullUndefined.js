cb4596969d72726cf73d84504f8f78394c8d64a9
Maicon Souza <maicon_souza09@hotmail.com>
2022-09-25 19:05:38 -0300

    25-09-22 ultima aula de hj

---
+++ 1-fundamentos/2-fundamentos/nullUndefined.js
@@ -0,0 +1,9 @@
+//Atribuição por valor 
+//Atribuição por referencia
+
+let valor  //Não inicializada
+console.log(valor)
+
+valor = null // ausência de valor
+// console.log(valor.toString()) vai dar erro
+
---
+++ 1-fundamentos/2-fundamentos/objetos.js
@@ -0,0 +1,12 @@
+const prod1 = {}
+
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Super Legal'] = 0.4 //evitar atributos com espaços
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
